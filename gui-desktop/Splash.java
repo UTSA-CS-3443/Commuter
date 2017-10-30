336bdd18ff79b5e9004e5b5b04e64c4e705db8dc
Jon Perry <26749062+dcjmp90@users.noreply.github.com>
2017-10-30 10:32:53 -0500

    commented methods and class, modified encapsulation

--- gui-desktop/Splash.java
+++ gui-desktop/Splash.java
@@ -8,21 +8,43 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+
+/**
+ * This is the init class of the java fx side of the program
+ * this class will encapsulate all the requirements of the init
+ * process to begin the application
+ *
+ * @author Jonathan Perry
+ */
 public class Splash extends Application{
 
-    public Stage stage;
-    public Scene scene;
+    private Stage stage;
+    private Scene scene;
 
     @FXML
     public ImageView image;
     public Pane pane;
 
+    /**
+     * the main method to start the process
+     * this will be only used to launch the javafx
+     *
+     * @param args
+     */
     public static void main(String[] args){
         launch(args);
     }
 
+    /**
+     * This method is to start the javafx program
+     * the program will start with a splash screen logo
+     * it will be used as a placeholder while connection is estab.
+     *
+     * @param primaryStage
+     * @throws Exception
+     */
     public void start(Stage primaryStage) throws Exception{
-        pane = new Pane();
+        this.pane = new Pane();
         Image img = new Image(getClass().getResource("splash.png").toString());
         image = new ImageView(img);
         pane.getChildren().add(image);
@@ -31,7 +53,7 @@ public class Splash extends Application{
         this.scene = new Scene(pane, 300, 200);
         this.scene.setFill(Color.TRANSPARENT);
         this.stage.initStyle(StageStyle.TRANSPARENT);
-        this.stage.setScene(scene);
+        this.stage.setScene(this.scene);
         this.stage.show();
     }
 
