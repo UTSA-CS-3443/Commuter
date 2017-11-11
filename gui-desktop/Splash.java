6ee9b1d914994c44b10f4ab27bad3d563dced8c4
dcjmp90 <axh733@my.utsa.edu>
2017-11-11 13:13:59 -0600

    newest edit of splash to main

--- gui-desktop/Splash.java
+++ gui-desktop/Splash.java
@@ -1,3 +1,5 @@
+package fxjava;
+
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
@@ -7,44 +9,22 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
-
-/**
- * This is the init class of the java fx side of the program
- * this class will encapsulate all the requirements of the init
- * process to begin the application
- *
- * @author Jonathan Perry
- */
 public class Splash extends Application{
 
-    private Stage stage;
-    private Scene scene;
+    public Stage stage, stage2;
+    public Scene scene;
 
     @FXML
     public ImageView image;
+    @FXML
     public Pane pane;
 
-    /**
-     * the main method to start the process
-     * this will be only used to launch the javafx
-     *
-     * @param args
-     */
-    public static void main(String[] args){
+    public static void main(String[] args) {
         launch(args);
     }
 
-    /**
-     * This method is to start the javafx program
-     * the program will start with a splash screen logo
-     * it will be used as a placeholder while connection is estab.
-     *
-     * @param primaryStage
-     * @throws Exception
-     */
     public void start(Stage primaryStage) throws Exception{
-        this.pane = new Pane();
+        pane = new Pane();
         Image img = new Image(getClass().getResource("splash.png").toString());
         image = new ImageView(img);
         pane.getChildren().add(image);
@@ -53,9 +33,14 @@ public class Splash extends Application{
         this.scene = new Scene(pane, 300, 200);
         this.scene.setFill(Color.TRANSPARENT);
         this.stage.initStyle(StageStyle.TRANSPARENT);
-        this.stage.setScene(this.scene);
+        this.stage.setScene(scene);
         this.stage.show();
+        Menu menu = new Menu();
+        this.stage2 = menu.getStage();
+        this.stage.close();
+        this.stage2.show();
     }
+
 
 
 }
